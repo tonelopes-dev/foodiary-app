761cc78416327b06836461dc64c3e70996328a7b
Tone Lopes <tonelopes.dev@gmail.com>
2025-09-03 15:13:21 -0300

    Adiciona o componente DateSwitcher para navegação entre datas, utilizando ícones e estilos do Tailwind CSS.

---
+++ src/components/DateSwitcher.tsx
@@ -0,0 +1,21 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react-native";
+import { colors } from "@/styles/colors";
+
+export function DateSwitcher() {
+  return (
+    <View className="flex-row items-center justify-between px-2 pt-3">
+      <TouchableOpacity className="size-12 items-center justify-center">
+        <ChevronLeftIcon size={20} color={colors.gray[700]} />
+      </TouchableOpacity>
+
+      <Text className="text-center font-sans-medium text-base tracking-[1.28px] text-gray-700">
+        HOJE, 20 DE SETEMBRO
+      </Text>
+
+      <TouchableOpacity className="size-12 items-center justify-center">
+        <ChevronRightIcon size={20} color={colors.gray[700]} />
+      </TouchableOpacity>
+    </View>
+  );
+}
