631e7da9ee9d280f929b7cdfdf4bc8096063764d
Tone Lopes <tonelopes.dev@gmail.com>
2025-09-03 15:13:26 -0300

    Atualiza o componente App para incluir suporte a fontes personalizadas, gerenciamento de tela de splash e reorganização da estrutura, integrando os componentes HomeHeader e DateSwitcher.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,46 @@
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import {
+  useFonts,
+  HostGrotesk_400Regular,
+  HostGrotesk_500Medium,
+  HostGrotesk_600SemiBold,
+  HostGrotesk_700Bold,
+} from "@expo-google-fonts/host-grotesk";
 import "./styles/global.css";
+import HomeHeader from "./components/HomeHeader";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { DateSwitcher } from "./components/DateSwitcher";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [loaded, error] = useFonts({
+    HostGrotesk_400Regular,
+    HostGrotesk_500Medium,
+    HostGrotesk_600SemiBold,
+    HostGrotesk_700Bold,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
-    <View className="bg-lime-500 flex-1 items-center justify-center">
-      <Text className="text-base">uhuuuu eu amo fazer Apps Mobile</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 bg-white">
+      <SafeAreaProvider>
+        <StatusBar style="auto" />
+        <HomeHeader />
+        <DateSwitcher />
+      </SafeAreaProvider>
     </View>
   );
 }
