b4dabcd4154915d99859c67cda03732225b1cf8f
Tone Lopes <tonelopes.dev@gmail.com>
2025-09-10 16:20:06 -0300

    Adiciona o componente AuthLayout, que fornece uma estrutura de layout para autenticação com ícones, título, subtítulo e área para filhos, melhorando a apresentação e a usabilidade na tela de login.

---
+++ src/components/AuthLayout.tsx
@@ -0,0 +1,43 @@
+import { Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+interface IAuthLayoutProps {
+  icon: string;
+  title: string;
+  subtitle: string;
+  children?: React.ReactNode;
+}
+
+export function AuthLayout({
+  icon,
+  title,
+  subtitle,
+  children,
+}: IAuthLayoutProps) {
+  const { bottom } = useSafeAreaInsets();
+
+  return (
+    <View className="flex-1">
+      <View className="flex-1 bg-white">
+        <View className="h-[98px] bg-lime-400" />
+
+        <View className="mx-auto -mt-6 size-12 items-center justify-center rounded-xl border-2 border-lime-400 bg-white">
+          <Text>{icon}</Text>
+        </View>
+
+        <View className="mx-auto mt-6 items-center">
+          <Text className="text-center font-sans-semibold text-[32px] tracking-[-0.32px] text-black-700">
+            {title}
+          </Text>
+          <Text className="mt-1 font-sans-regular text-base text-gray-700">
+            {subtitle}
+          </Text>
+        </View>
+
+        <View className="mt-10 flex-1 px-6" style={{ marginBottom: bottom }}>
+          {children}
+        </View>
+      </View>
+    </View>
+  );
+}
