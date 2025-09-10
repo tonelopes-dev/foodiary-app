23fedfe935b5452c3e164f10cad4398ac4ca43f9
Tone Lopes <tonelopes.dev@gmail.com>
2025-09-10 16:19:38 -0300

    Refatora o componente CreateMealBottomBar para renomear o estado do modal da câmera para 'isPictureModalOpen' e ajusta a classe CSS para melhorar a apresentação, mantendo a funcionalidade de abertura dos modais de áudio e câmera.

--- src/components/CreateMealBottomBar.tsx
+++ src/components/CreateMealBottomBar.tsx
@@ -1,21 +1,23 @@
 import { CameraIcon, MicIcon } from "lucide-react-native";
-import React, { useState } from "react";
+import { useState } from "react";
 import { View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { AudioModal } from "./AudioModal";
 import { Button } from "./Button";
+import { CameraModal } from "./CameraModal";
 
 export function CreateMealBottomBar() {
   const { bottom } = useSafeAreaInsets();
 
   const [isAudioModalOpen, setIsAudioModalOpen] = useState(false);
-  const [isCameraModalOpen, setIsCameraModalOpen] = useState(false);
+  const [isPictureModalOpen, setIsPictureModalOpen] = useState(false);
+
   return (
     <View
-      className="absolute bottom-0 left-0 right-0 border-t border-gray-200 bg-white px-5 pt-3"
+      className="absolute bottom-0 z-10 w-full border-t border-gray-400 bg-white"
       style={{ height: 80 + bottom }}
     >
-      <View className="flex-row justify-center gap-3">
+      <View className="mx-auto mt-4 flex-row gap-4">
         <Button
           size="icon"
           color="gray"
@@ -23,24 +25,24 @@ export function CreateMealBottomBar() {
         >
           <MicIcon />
         </Button>
+
         <Button
           size="icon"
           color="gray"
-          onPress={() => setIsCameraModalOpen(true)}
+          onPress={() => setIsPictureModalOpen(true)}
         >
           <CameraIcon />
         </Button>
       </View>
 
-      {/* <AudioModal open={isAudioModalOpen} onClose={() => setIsAudioModalOpen(false)} />
-      <CameraModal open={isCameraModalOpen} onClose={() => setIsCameraModalOpen(false)} /> */}
       <AudioModal
         open={isAudioModalOpen}
         onClose={() => setIsAudioModalOpen(false)}
       />
+
       <CameraModal
-        open={isCameraModalOpen}
-        onClose={() => setIsCameraModalOpen(false)}
+        open={isPictureModalOpen}
+        onClose={() => setIsPictureModalOpen(false)}
       />
     </View>
   );
