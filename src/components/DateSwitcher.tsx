fd1a29397727b290f5568bb39612caab5038d44f
Tone Lopes <tonelopes.dev@gmail.com>
2025-09-10 16:19:27 -0300

    Refatora o componente DateSwitcher para aceitar propriedades de data atual e funções de navegação, melhorando a flexibilidade e a apresentação da data formatada.

--- src/components/DateSwitcher.tsx
+++ src/components/DateSwitcher.tsx
@@ -1,20 +1,37 @@
-import { View, Text, TouchableOpacity } from "react-native";
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react-native";
-import { colors } from "@/styles/colors";
+import { Text, TouchableOpacity, View } from "react-native";
+import { colors } from "../styles/colors";
+import { formatDate } from "@/utils/formatDate";
 
-export function DateSwitcher() {
+interface IDateSwitcherProps {
+  currentDate: Date;
+  onPreviousDate(): void;
+  onNextDate(): void;
+}
+
+export function DateSwitcher({
+  currentDate,
+  onNextDate,
+  onPreviousDate,
+}: IDateSwitcherProps) {
   return (
-    <View className="flex-row items-center justify-between pt-3">
-      <TouchableOpacity className="size-12 items-center justify-center">
-        <ChevronLeftIcon size={25} color={colors.gray[700]} />
+    <View className="mt-3 flex-row items-center justify-between px-2">
+      <TouchableOpacity
+        className="size-12 items-center justify-center"
+        onPress={onPreviousDate}
+      >
+        <ChevronLeftIcon size={20} color={colors.black[700]} />
       </TouchableOpacity>
 
-      <Text className="text-center font-sans-medium text-base tracking-[1.28px] text-gray-700">
-        HOJE, 20 DE SETEMBRO
+      <Text className="font-sans-medium text-base tracking-[1.28px] text-gray-700">
+        {formatDate(currentDate)}
       </Text>
 
-      <TouchableOpacity className="size-12 items-center justify-center">
-        <ChevronRightIcon size={25} color={colors.gray[700]} />
+      <TouchableOpacity
+        className="size-12 items-center justify-center"
+        onPress={onNextDate}
+      >
+        <ChevronRightIcon size={20} color={colors.black[700]} />
       </TouchableOpacity>
     </View>
   );
