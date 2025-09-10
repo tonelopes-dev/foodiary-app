eaad0fe88dc847a3a02fa1ea5154b7c0a5b19b54
Tone Lopes <tonelopes.dev@gmail.com>
2025-09-10 16:19:11 -0300

    Refatora o componente MealCard para incluir a data de criação, ícone e lista de alimentos, melhorando a apresentação das informações da refeição e a navegação para detalhes.

--- src/components/MealCard.tsx
+++ src/components/MealCard.tsx
@@ -1,37 +1,38 @@
+import { Link } from "expo-router";
 import { Text, TouchableOpacity, View } from "react-native";
+import { formatMealDate } from "@/utils/formatMealDate";
+
 interface IMealCardProps {
   id: string;
+  createdAt: Date;
+  icon: string;
   name: string;
-  time: string;
-  description: string;
-  withinDiet: boolean;
+  foods: { name: string }[];
 }
-export function MealCard({
-  id,
-  name,
-  time,
-  description,
-  withinDiet,
-}: IMealCardProps) {
-  return (
-    <TouchableOpacity className="w-full">
-      <Text className="font-sans-regular text-base text-gray-700">
-        Hoje, 12h20
-      </Text>
-      <View className="mt-2 flex-row gap-3 rounded-2xl border border-gray-400 px-4 py-5">
-        <View className="size-12 items-center justify-center rounded-full bg-gray-200">
-          <Text>🍞</Text>
-        </View>
 
-        <View className="flex-1">
-          <Text className="font-sans-regular text-base text-gray-700">
-            {name}
-          </Text>
-          <Text className="font-sans-medium text-base text-black-700">
-            {description}
-          </Text>
+export function MealCard({ createdAt, foods, icon, id, name }: IMealCardProps) {
+  return (
+    <Link href={`/meals/${id}`} asChild>
+      <TouchableOpacity>
+        <Text className="font-sans-regular text-base text-gray-700">
+          {formatMealDate(createdAt)}
+        </Text>
+
+        <View className="mt-2 flex-row items-center gap-3 rounded-2xl border border-gray-400 px-4 py-5">
+          <View className="size-12 items-center justify-center rounded-full bg-gray-200">
+            <Text>{icon}</Text>
+          </View>
+
+          <View>
+            <Text className="font-sans-regular text-base text-gray-700">
+              {name}
+            </Text>
+            <Text className="font-sans-medium text-base text-black-700">
+              {foods.map(({ name }) => name).join(", ")}
+            </Text>
+          </View>
         </View>
-      </View>
-    </TouchableOpacity>
+      </TouchableOpacity>
+    </Link>
   );
 }
